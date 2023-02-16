be00c2d12d41b09d47444dcbe52cdf2f671bf8c3
Immaculate103 <immaculateasachi@gmail.com>
2023-02-16 13:56:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - print a line of code using put function
  *
- * Return = 0(success)
+ * Return: 0
 */
 int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
        	return (0);
 }	
