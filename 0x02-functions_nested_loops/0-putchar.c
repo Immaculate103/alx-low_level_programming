7fcdfa0fd5ded9bba1e14de8b6d0bdf7e7ced1cf
Immaculate103 <immaculateasachi@gmail.com>
2023-02-21 04:33:21 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - prints _putchar, followed by a new line
  *
@@ -15,6 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
