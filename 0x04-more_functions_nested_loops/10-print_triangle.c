b0240d6d7e8eb6480cd6bd6d032b56ee82294b8e
Immaculate103 <immaculateasachi@gmail.com>
2023-02-23 11:09:52 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *print_triangle - prints triangles
+ * *@size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 1; j >= 0; j--)
+			{
+				_putchar(32);
+			}
+			for (j = i; j < size; j++)
+			{
+				_putchar(35);
+			}
+		}
+		_putchar('\n');
+	}
+}
