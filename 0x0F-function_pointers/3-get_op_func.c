50b3db3f03ea7674c39f87acd43fe5a5eff74b9e
Immaculate103 <immaculateasachi@gmail.com>
2023-03-22 13:19:57 -0700

    functions

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,32 +1,31 @@
-#include "3-calc.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
+#include "3-calc.h"
 /**
- * get_op_func - ...
- * @s: ...
- *
- * Return: ...
+ * get_op_func - gets the right func
+ * @s: function s
+ * Return: int value
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div },
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
-	int i = 0;
+	int index;
 
-	while (i < 5)
+	index = 0;
+	while (ops[index].f != NULL)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-
-		i++;
+		if (*s == *(ops[index].op) && s[1] == '\0')
+			return (ops[index].f);
+		index++;
 	}
 
-	return (0);
+	printf("Error\n");
+	exit(99);
 }
