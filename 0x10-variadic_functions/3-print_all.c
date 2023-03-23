41a60583e62b7c9c9da715dc915d9299313cc2a2
Immaculate103 <immaculateasachi@gmail.com>
2023-03-23 12:18:26 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,47 +4,37 @@
  * @format: specifies the necessary operations
  * Return: void
  */
-void print_all(const char * const format, ...)
-{
-	int i;
-	int flag;
-	char *str;
-	va_list a_list;
+void print_all(const char * const format, ...) {
+    const char *p = format;
+    va_list ap;
+    va_start(ap, format);
 
-	va_start(a_list, format);
-	 i = 0;
-	 while (format != NULL && format[i] != '\0')
-	 {
-		 switch (format[i])
-		 {
-			 case 'c':
-				 printf("%c", va_arg(a_list, int));
-				 flag = 0;
-				 break;
-			 case 'i':
-				 printf("%i", va_arg(a_list, int));
-				 flag = 0;
-				 break;
-			 case 'f':
-				 printf("%f", va_arg(a_list, double));
-				 flag = 0;
-				 break;
-			 case 's':
-				 str = va_arg(a_list, char*);
-				 if (str == NULL)
-					 str = "(nil)";
-				 printf("%s", str);
-				 flag = 0;
-				 break;
-			 default:
-				 flag = 1;
-				 break;
-		 }
-		 if (format[i + 1] != '\0' && flag == 0)
-			 printf(", ");
-		 i++;
-	 }
-	 printf("\n");
-	 va_end(a_list);
+    char c;
+    int i;
+    float f;
+    char *s;
+
+    while (*p != '\0') {
+        if (*p == 'c') {
+            c = (char)va_arg(ap, int);
+            printf("%c", c);
+        } else if (*p == 'i') {
+            i = va_arg(ap, int);
+            printf("%d", i);
+        } else if (*p == 'f') {
+            f = (float)va_arg(ap, double);
+            printf("%f", f);
+        } else if (*p == 's') {
+            s = va_arg(ap, char *);
+            if (s == NULL) {
+                printf("(nil)");
+            } else {
+                printf("%s", s);
+            }
+        }
+        p++;
+    }
+
+    printf("\n");
+    va_end(ap);
 }
-
