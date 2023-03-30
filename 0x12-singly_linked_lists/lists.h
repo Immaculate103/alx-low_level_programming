91293b21ea1db341354ead526d81876296ded648
Immaculate103 <immaculateasachi@gmail.com>
2023-03-30 12:20:35 -0700

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
-lists.h
+#ifndef LINKED
+#define LINKED
 
-#ifndef LISTS_H
-#define LISTS_H
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -9,14 +11,13 @@ lists.h
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description: singly linked list node structure for Holberton project
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -25,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /*LINKED*/
