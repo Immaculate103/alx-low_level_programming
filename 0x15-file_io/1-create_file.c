e76bdbbc762b256d92419de0ddc32f846f5af14c
Immaculate103 <immaculateasachi@gmail.com>
2023-04-10 14:54:58 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,20 +11,26 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 
 	if (filename == NULL)
+	{
 		return (-1);
+	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
+	{
 		return (-1);
+	}
 
 	if (text_content != NULL)
 	{
 		ssize_t write_result = write(fd, text_content, strlen(text_content));
 
 		if (write_result == -1)
+		{
 			close(fd);
-		return (-1);
+			return (-1);
+		}
 	}
 
 	close(fd);
