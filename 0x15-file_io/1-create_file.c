865a9a562c16ba2170738b279640d44441de1dff
Immaculate103 <immaculateasachi@gmail.com>
2023-04-10 14:50:40 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_file - formulates a file
+ * @text_content: files writing string that is NULL terminated
+ * @filename: file name to be formulated
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		ssize_t write_result = write(fd, text_content, strlen(text_content));
+
+		if (write_result == -1)
+			close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
+
