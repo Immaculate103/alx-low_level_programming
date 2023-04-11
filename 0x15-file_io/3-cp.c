3593d1916a2112c1e7525684284ea81a81c65d10
Immaculate103 <immaculateasachi@gmail.com>
2023-04-10 17:05:22 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,6 +7,62 @@
 
 #define BUFFER_SIZE 1024
 /**
+ * copy_file - copies the content of one file to another
+ *
+ * @file_from: Source file
+ * @file_to: Destination file
+ *
+ * Return: 0 on success, exit with error code on failure
+ *
+ */
+int copy_file(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to, len;
+	char buffer[BUFFER_SIZE];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Cant't read from file %s\n", file_from);
+		return (-1);
+	}
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Cant't write to %s\n", file_to);
+		close(fd_from);
+		return (-1);
+	}
+	while ((len = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (write(fd_to, buffer, len) != len)
+		{
+			dprintf(STDERR_FILENO, "Error: Short write to %s\n", file_to);
+			close(fd_from);
+			close(fd_to);
+			return (-1);
+		}
+	}
+	if (len == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd_from);
+		close(fd_to);
+		return (-1);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		return (-1);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Cant't close fd %d\n", fd_to);
+		return (-1);
+	}
+	return (0);
+}
+/**
  * main - Entry point function that copies the content of files
  *
  * @argc: Number of arguments passed to the program
@@ -17,50 +73,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, len;
-	char buffer[1024];
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
+		return (97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	if (copy_file(argv[1], argv[2]) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Cant't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while ((len = read(fd_from, buffer, 1024)) > 0)
-	{
-		if (write(fd_to, buffer, len) != len)
-		{
-			dprintf(STDERR_FILENO, "Error: Short write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-	if (len == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Cant't close fd %d\n", fd_to);
-		exit(100);
+		return (99);
 	}
 	return (0);
 }
-
-
